5e53c19749e7f3b437c9a241412128e7589090a3
DominicMartinez <domvm.7@gmail.com>
2024-11-23 12:55:08 -0800

    starting backend

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,7 +7,7 @@ import "./Main.css";
 const Main = ({ itemCards, onDelete }) => {
   return (
     <main className="main">
-      <h1 className="main__title">Main Content</h1>
+      <h1 className="main__title">Main</h1>
       <div className="item-cards-container">
         {itemCards.map((card) => (
           <ItemCard key={card.id} onDelete={() => onDelete(card.id)} />
