4eb248144826f94b30ba9101ec13663940801eb4
DominicMartinez <domvm.7@gmail.com>
2024-11-23 16:51:55 -0800

    fixed about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,10 +6,10 @@ const About = () => {
     <section className="about">
       <h2 className="about__title">About This Project</h2>
       <p className="about__description">
-        This project is a dynamic web application that allows users to create,
-        edit, and delete custom item cards. It includes user authentication,
-        leveraging third-party APIs for enhanced functionality and offers a
-        responsive user interface for seamless interaction.
+        This project is a dynamic fullstack web application that allows users to
+        create, edit, and delete custom item cards. It includes user
+        authentication, leveraging third-party APIs for enhanced functionality
+        and offers a responsive user interface for seamless interaction.
       </p>
     </section>
   );
