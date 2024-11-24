f659085e43e35bb75822d881ab5068aa65560bb4
DominicMartinez <domvm.7@gmail.com>
2024-11-23 16:51:02 -0800

    added about to main page

---
+++ src/components/About/About.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <section className="about">
+      <h2 className="about__title">About This Project</h2>
+      <p className="about__description">
+        This project is a dynamic web application that allows users to create,
+        edit, and delete custom item cards. It includes user authentication,
+        leveraging third-party APIs for enhanced functionality and offers a
+        responsive user interface for seamless interaction.
+      </p>
+    </section>
+  );
+};
+
+export default About;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@
 
 import React from "react";
 import ItemCard from "../ItemCard/ItemCard";
+import About from "../About/About";
 import "./Main.css";
 
 const Main = ({ itemCards, onDelete }) => {
   return (
     <main className="main">
-      <h1 className="main__title">Main</h1>
+      <About />
+      <h1 className="main__title">Main Content</h1>
       <div className="item-cards-container">
         {itemCards.map((card) => (
           <ItemCard key={card.id} onDelete={() => onDelete(card.id)} />
