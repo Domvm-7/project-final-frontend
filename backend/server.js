392cf5f41a4437bcfffd5a3f5f08af7685719881
DominicMartinez <domvm.7@gmail.com>
2024-12-09 17:40:01 -0800

    working on reviewers request

--- backend/server.js
+++ backend/server.js
@@ -10,9 +10,9 @@ const cors = require("cors");
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // or "../frontend/build"
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html")); // or "../frontend/build"
   });
 }
 
