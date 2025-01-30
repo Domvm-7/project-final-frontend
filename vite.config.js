488511a178797c1cc71de9b72c1325552bb198a3
DominicMartinez <domvm.7@gmail.com>
2025-01-29 16:32:18 -0800

    fixing port

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001, // Change to your desired port
+    port: parseInt(process.env.VITE_PORT) || 3001, // Use .env variable or fallback
   },
 });
